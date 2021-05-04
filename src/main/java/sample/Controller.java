cf54e7ad455b9fbc4c17a50ecd5efaf06c8384a5
Pranshu Tiwari <pranshut2001@gmail.com>
2021-05-04 23:30:30 +0530

    Calculator Layout

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,5 +1,14 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.Initializable;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
 }
