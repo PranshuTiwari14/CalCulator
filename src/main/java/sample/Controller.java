2cd57c23fe8f302c0e412851a67539ccab1c226c
Pranshu Tiwari <pranshut2001@gmail.com>
2021-05-04 23:46:26 +0530

    Intialized all methods in controller

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,14 +1,41 @@
 package sample;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+    @FXML
+    private JFXTextField display;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+
+    }
+
+
+    public void onDigitPress(ActionEvent actionEvent) {
+    }
+
+    public void onAddPress(ActionEvent actionEvent) {
+    }
+
+    public void onSubtractPress(ActionEvent actionEvent) {
+    }
+
+    public void onMultiplyPress(ActionEvent actionEvent) {
+    }
+
+    public void onEqualPress(ActionEvent actionEvent) {
+    }
+
+    public void onDivPress(ActionEvent actionEvent) {
+    }
+
+    public void onClearPress(ActionEvent actionEvent) {
     }
 }
